711b3b01a6e03e6e9746010f0bfa6795e8737a52
Ubuntu <ubuntu@ip-172-31-3-83.ec2.internal>
2021-05-29 05:43:24 +0000

    Add oauth2

---
+++ src/main/java/br/com/fiap/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package br.com.fiap.repository;
+
+import br.com.fiap.domain.Authority;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+
+/**
+ * Spring Data R2DBC repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends R2dbcRepository<Authority, String> {}
