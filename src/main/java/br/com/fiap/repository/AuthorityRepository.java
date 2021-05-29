976a486498fddbee6f6c988a198be272c9fdc27c
Ubuntu <ubuntu@ip-172-31-3-83.ec2.internal>
2021-05-29 05:34:49 +0000

    ajustando repositorio

--- src/main/java/br/com/fiap/repository/AuthorityRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.repository;
-
-import br.com.fiap.domain.Authority;
-import org.springframework.data.r2dbc.repository.R2dbcRepository;
-
-/**
- * Spring Data R2DBC repository for the {@link Authority} entity.
- */
-public interface AuthorityRepository extends R2dbcRepository<Authority, String> {}
--- src/main/java/br/com/fiap/web/rest/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Spring MVC REST controllers.
- */
-package br.com.fiap.web.rest;
